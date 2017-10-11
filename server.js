549ee443ef9d4678c7ad41d960da08e06f5d361a
ijohnnyh <ijohnnyh@live.unc.edu>
2017-10-11 11:46:58 -0400

    Modified JSON printout

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var gFinance = require('google-finance');
 var util = require('util');
+var request 
 
 var SYMBOL = 'EFX:NYSE';
 
@@ -13,13 +14,14 @@ app.get('/', function(req,res){
 		symbol: SYMBOL
 	}, function (err, news) {
 		if (err) { throw err; }
-		var info = `=== ${SYMBOL} ${news.length} ===`
+		var info = `=== ${SYMBOL} ${news.length} RESULTS===`
 		console.log(
 			'\x1b[31m%s\x1b[0m', info
 			);
 		if (news[0]) {
 			for (var i = 0; i < news.length; i++){
-				console.log('%s\n', JSON.stringify(news[i],null,2));
+				console.log(JSON.stringify(news[i]['title'],null,2))
+				//console.log('%s\n', JSON.stringify(news[i],null,2));
 			}
 		} else {
 			console.log('N/A');
